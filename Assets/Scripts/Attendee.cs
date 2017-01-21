f4e910c4f4336b5514a6fade0e2045e964d63a06
Pierre-Olivier Chartrand <pierreolivier.chartrand@gmail.com>
2017-01-21 17:56:02 -0500

    Added camera anim

--- Assets/Scripts/Attendee.cs
+++ Assets/Scripts/Attendee.cs
@@ -20,6 +20,7 @@ public class Attendee : MonoBehaviour {
     private NavMeshAgent nav;
     private float emoteCD;
     public Vector3 finalLocation;
+    private Animator anim;
 
     public bool leaving;
     public Attendee() {
@@ -35,6 +36,7 @@ public class Attendee : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
+        anim = GetComponent<Animator>();
         Setup();
         if (!nav)
             nav = GetComponent<NavMeshAgent>();
@@ -43,18 +45,25 @@ public class Attendee : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         CalculateHappiness();
+        anim.SetBool("Walking", false);
+        anim.SetBool("Dancing", false);
         if (finalLocation != -1 * Vector3.one) {
             if (Vector3.Distance(nav.destination, transform.position) < 3f) {
                 nav.SetDestination(finalLocation);
                 finalLocation = -1 * Vector3.one;
+                anim.SetBool("Walking", true);
+
+
             }
         }	else if (leaving) {
             if (Vector3.Distance(nav.destination, transform.position) < 3f) {
+                anim.SetBool("Walking", true);
                 Destroy(gameObject, 0.3f);
             }
         }
         mood = GetMood();
-        if(emoteCD<=0)
+        anim.SetBool("Dancing", mood!=Mood.Sad);
+        if (emoteCD<=0)
         {
             emoteCD = timeBetweenEmotes+Random.Range(0,3);
             if(mood==Mood.Happy)
@@ -73,7 +82,7 @@ public class Attendee : MonoBehaviour {
             }
         }
         emoteCD -= Time.deltaTime;
-
+        
 	}
     Mood GetMood()
     {
